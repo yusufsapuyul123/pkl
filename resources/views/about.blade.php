394863fee400769a21c1a60c38fe9518ddcc16f3
pkl <yusufsapuyul@gmail.com>
2020-09-30 14:33:39 +0700

    first

---
+++ resources/views/about.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="/">Home</a>
+      <a class="nav-item nav-link" href="/about">About</a>
+      <a class="nav-item nav-link" href="/data">Data pasien</a>
+      
+    </div>
+  </div>
+  
+</nav>
+  </head>
+  <div class="jumbotron">
+  <h1 class="display-4">COVCARE</h1>
+  <p class="lead">This is one of helper aplication for deseasing COVID-19.</p>
+  <p class="lead">ABOUT US</p>
+  <div class="container text-right">
+  <a class="btn btn-primary btn-md" href="#" role="button">Learn more</a>
+  <hr class="my-4">
+  
+  
+  <body>
+
+  <div class="container">
+  <div class="row">
+  <div class="col-10">
+    <h5 class="mt-3">COVCARE adalah salah satu aplikasi yang membantu melayani masyarakat sekitar
+    baik itu info terupdate ataupun rumah sakit rujukan bagi pasien yang terinfeksi COVID-19.</h5>
+    
+  </div>
+  </div>
+  </div>
+</div>
+  </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
+<!-- As a heading -->
+<nav class="navbar navbar-light bg-light">
+  <span class="navbar-brand mb-0 h1"></span>
+</nav>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<div class="jumbotron">
+  <h1 class="display-4">COVCARE</h1>
+  <p class="lead">This is one of helper aplication for deseasing COVID-19.</p>
+  <hr class="my-4">
+  <div class="container text-right">
+  <a class="btn btn-primary btn-md" href="details" role="button">Learn more</a>
+  <a href="login"><button type="button" class="btn btn-primary text-center">Login</button></a>
+  </div>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+      
+      <title>Hello, world!</title>
+    </head>
+    
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active" href="/">Home</a>
+              <a class="nav-item nav-link" href="/about">About</a>
+              <a class="nav-item nav-link" href="/data">Data pasien</a>
+              
+    <body>
+      
+              
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+          <div class="col-10">
+            <style type="text/css">
+          </div>
+  h1{
+    color:black;
+  }
+  
+  }
+  </style>   
+          
+          <div class="container">
+            <div class="row">
+        <div class="col text-center">
+          <h1 class="mt-3 " ></h1>
+          <img src="assets/img/2.jpg" width="900" alt=""> 
+          <h3 class="mt-2 ">Aplikasi covid yang menyediakan layanan</h3>
+          <h3 class="mt-1"size="5px" >serta info upgrade COVID-19</h3>
+          <hr size="10px" color="black">
+        </div>
+      </div>
+    </div>
+
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="assets/images/1.jpg" alt="Rumah sakit rujukan">
+    <div class="card-body">
+    <a href="details"><h5 class="card-title">RSUD.BANTEN</h5></a>
+      <p class="card-text">Jalan Syeh Nawawi Al Bantani, Kelurahan Banjarsari, Kecamatan Cipocok Jaya, Banjarsari, Serang, Banjarsari, Kec. Cipocok Jaya, Kota Serang, Banten 42123.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="assets/2.jpg" alt="Rumah sakit rujukan">
+    <div class="card-body">
+    <a href="details"><h5 class="card-title">RS.BALARAJA</h5></a>
+      <p class="card-text">Jl. Rumah Sakit No.88, Tobat, Kec. Balaraja, Tangerang, Banten 15126.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="pola hidup sehat">
+    <div class="card-body">
+      <a href="details"><h5 class="card-title">AYO KITA BIASAKAN HIDUP SEHAT</h5></a>
+      <p class="card-text">jaga jarak aman,agar tidak terinfeksi gejala covid,cuci tangan dengan air mengalir  dan dengan sabun agar bakteri jahat mati,menggunkan masker kemanapun anda pergi.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+        
+          
+
+          
+
+        
+          </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
+
